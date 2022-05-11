548bc0fed8bcc4776ceacca1d57fb7fa7e73054a
RiteshWadge <104705529+RiteshWadge@users.noreply.github.com>
2022-05-11 20:53:00 +0530

    Initial commit

---
+++ Student_Enrollment_Form/script.js
@@ -0,0 +1,39 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click",displayDetails);
+
+var row = 1;
+
+function displayDetails(){
+	var name = document.getElementById("name").value;
+	var mail = document.getElementById("mail").value;
+	var age = document.getElementById("age").value;
+	var web= document.getElementById("web").value;
+	var img = document.getElementById("file").value;
+
+
+	if(!name || !mail || !age || !web || !file  ){
+		alert("please fill all the details");
+		return;
+	}
+
+	var display = document.getElementById("display");
+
+	var newRow = display.insertRow(row);
+
+	var cell1 = newRow.insertCell(0);
+	var cell2 = newRow.insertCell(1);
+	var cell3 = newRow.insertCell(2);
+	var cell4 = newRow.insertCell(3);
+	var cell5 = newRow.insertCell(4);
+
+
+
+	cell1.innerHTML = name;	
+	cell2.innerHTML = mail;	
+	cell3.innerHTML = age;	
+	cell4.innerHTML = web;	
+	cell5.innerHTML = file;	
+		
+
+	
+}
